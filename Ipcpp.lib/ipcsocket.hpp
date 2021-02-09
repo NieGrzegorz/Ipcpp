397799b7b87b4505541e5bebc6589a74b6526414
NieGrzegorz <grzegorz0711@gmail.com>
2021-02-09 18:40:46 +0100

    Add project files.

---
+++ Ipcpp.lib/ipcsocket.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+namespace ipcpp
+{
+	class BaseSocket
+	{
+
+	};
+
+	class ServerSocket : public BaseSocket
+	{
+
+
+	};
+
+	class ClientSocket : public BaseSocket
+	{
+
+
+	};
+
+}
